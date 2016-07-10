ed5062b224749187a2bc3724d298c8fd62aacc61
gap <khiemtonthat@yahoo.de>
2016-07-10 14:40:14 +0200

    Jetzt kann man sich das Bild per GET request Pull

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ var bodyParser = require("body-parser");
 var app = express();
 var fs = require('fs'),
     url = require('url');
+var jsonBild;
 
 app.use('/', express.static(__dirname + '/'));
 app.use(bodyParser.json({
     limit: '5 mb' //max. size
 }));
+
 app.use(bodyParser.urlencoded({
     extended: false,
     limit: '5mb' //max. size
@@ -17,14 +19,27 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+//sends the Bitmap as a String on a GET request
 app.get('/bild', function(req, res) {
-    res.send('dsfaf');
+  //deletes the old reference to the json
+  delete require.cache[require.resolve('./public/bild.json')]
+  jsonBild = require('./public/bild.json')
+  res.send(jsonBild.bild);
 });
 
+app.post('/test', function(req, res) {
+    res.send('Server received String');
+});
+
+// gets a picture and saves it as a String in .js; .json
 app.post('/bild', function(req, res) {
-    console.log(req.body.bild);
     fs.writeFile(__dirname + '/public/data.js',
         "var data = " + JSON.stringify({
+            bild: new String(req.body.bild)
+        })
+    );
+    fs.writeFile(__dirname + '/public/bild.json',
+        JSON.stringify({
             bild: new String(req.body.bild)
         })
     );
